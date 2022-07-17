7e284f4518707d91c31dbd141029f9e6fa45fe02
gBL17 <fgrodrigues.gabriel@gmail.com>
2022-07-16 23:39:55 -0300

    feat(create-new-user) create new user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './navbar/navbar.component';
+import { CreateNewUserComponent } from './create-new-user/create-new-user.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      NavBarComponent
+      NavBarComponent,
+      CreateNewUserComponent
    ],
   imports: [
     BrowserModule
